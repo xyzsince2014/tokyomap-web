2a5143ea8db9f1bba4d76d5bef7ab17bb3013365
xyzsince2014 <xyzsince2014@gmail.com>
2020-05-03 11:15:34 +0900

    refactored LeafletMap

---
+++ src/components/LeafletMap/LeafletMap.tsx
@@ -0,0 +1,28 @@
+/** @jsx jsx */
+import * as React from 'react';
+import {jsx} from '@emotion/core';
+import {Map, Marker, Popup, TileLayer} from 'react-leaflet';
+import {LatLngTuple} from 'leaflet';
+
+export interface LeafletMapProps {
+  latLng?: LatLngTuple;
+  zoom?: number;
+}
+
+const LeafletMap: React.FC<LeafletMapProps> = ({latLng = [35.680722, 139.767271], zoom = 10}) => (
+  <Map id="map" center={latLng} zoom={zoom}>
+    <TileLayer
+      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+    />
+    <Marker position={latLng}>
+      <Popup>
+        A pretty CSS3 popup.
+        <br />
+        Easily customizable.
+      </Popup>
+    </Marker>
+  </Map>
+);
+
+export default LeafletMap;

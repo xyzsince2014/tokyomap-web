44ed0d2abe8485ba5b7bc017b5b5026a2f28779a
Ryuta Yamamoto <30502252+xyzsince2014@users.noreply.github.com>
2020-05-06 23:28:34 +0900

    added flocss (#1)
    * added scss files. malfunctioning
    
    * added flocss into src/assets/scss
    
    * revised README
    
    * modified imports
    
    * odifed map layout
    
    * modified scss of the clock on the leaflet-bottom
    
    * modified imports

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,6 @@ import ReactDOM from 'react-dom';
 
 import App from './components/App';
 
-import './assets/scss/parts/_base.scss';
+import './assets/scss/base.scss';
 
 ReactDOM.render(<App />, document.getElementById('root') as HTMLElement);

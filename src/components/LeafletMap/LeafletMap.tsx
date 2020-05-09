658b00bae3354ece21dcd97823116a6fd4b1e78b
xyzsince2014 <xyzsince2014@gmail.com>
2020-05-09 10:48:27 +0900

    enabled displaying multiple positons

--- src/components/LeafletMap/LeafletMap.tsx
+++ src/components/LeafletMap/LeafletMap.tsx
@@ -5,21 +5,31 @@ import {LatLngTuple} from 'leaflet';
 import Clock from './Clock';
 
 export interface LeafletMapProps {
-  latLng?: LatLngTuple;
+  centre?: LatLngTuple;
   zoom?: number;
+  positions?: LatLngTuple[];
 }
 
-const LeafletMap: React.FC<LeafletMapProps> = ({latLng = [35.680722, 139.767271], zoom = 11}) => (
-  <Map className="l-leafletmap" center={latLng} zoom={zoom}>
+const LeafletMap: React.FC<LeafletMapProps> = ({
+  centre = [35.680722, 139.767271],
+  zoom = 11,
+  positions = [
+    [35.7101, 139.8107],
+    [35.7121, 139.8207],
+  ],
+}) => (
+  <Map className="l-leafletmap" center={centre} zoom={zoom}>
     <TileLayer
       url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
     />
-    <Marker position={latLng}>
-      <Popup>
-        A pretty CSS3 popup.<span className="u-phrase">Easily customizable.</span>
-      </Popup>
-    </Marker>
+    {positions.map(pos => (
+      <Marker position={pos}>
+        <Popup>
+          A pretty CSS3 popup.<span className="u-phrase">Easily customizable.</span>
+        </Popup>
+      </Marker>
+    ))}
     <div className="l-leafletmap__bottom">
       <Clock />
     </div>

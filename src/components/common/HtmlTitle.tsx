8f0c64950b83b8ac04a7886992f1d44ac0f2f890
xyzsince2014 <xyzsince2014@gmail.com>
2020-05-09 10:11:16 +0900

    removed emotion

--- src/components/common/HtmlTitle.tsx
+++ src/components/common/HtmlTitle.tsx
@@ -1,6 +1,4 @@
-/** @jsx jsx */
 import * as React from 'react';
-import {jsx} from '@emotion/core';
 import {Helmet} from 'react-helmet';
 
 const HtmlTitle: React.FC<{title?: string}> = ({title = 'hoge'}) =>

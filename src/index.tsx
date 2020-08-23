45a5f0530164520d85f6fa5b3571cab7e135b34a
xyzsince2014 <xyzsince2014@gmail.com>
2020-08-24 01:10:24 +0900

    renamed src/components/App.tsx to src/App.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,7 @@ import {Provider} from 'react-redux';
 import {applyMiddleware, compose, createStore} from 'redux';
 import createSagaMiddleware from 'redux-saga';
 
-import App from './components/App';
+import App from './App';
 import rootReducer from './reducers/rootReducer';
 // import rootSaga from './sagas/github';
 

fa9b5a889320d277c943fbcf53695ed241bc2589
xyzsince2014 <xyzsince2014@gmail.com>
2020-08-30 20:07:45 +0900

    refactored Router and Auth components

--- src/Router.tsx
+++ src/Router.tsx
@@ -4,21 +4,15 @@ import {Redirect, Route, Switch} from 'react-router';
 
 // import pages from './pages';
 
-import SignIn from './components/Auth/SignIn';
 import Auth from './containers/Auth/Auth';
 import LeafletMap from './components/LeafletMap/LeafletMap';
 
 const Router: React.FC<{}> = () => (
   <BrowserRouter>
     <Switch>
-      <Route path="/auth" component={SignIn} />
-      {/* <Redirect to="/auth/signin" /> */}
       <Auth>
-        <Switch>
-          <Route path="/" component={LeafletMap} />
-          {/* <Route path="/auth/signout" component={SingOut}/> */}
-          {/* <Redirect to="/" /> */}
-        </Switch>
+        <Route path="/" component={LeafletMap} />
+        <Redirect to="/" />
       </Auth>
     </Switch>
   </BrowserRouter>

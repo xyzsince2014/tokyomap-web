16766b967cedef7d1bb640f150a8e7b52b867faf
xyzsince2014 <xyzsince2014@gmail.com>
2020-09-02 00:24:07 +0900

    removed unncessary comments

--- src/containers/LeafletMap/LeafletMap.tsx
+++ src/containers/LeafletMap/LeafletMap.tsx
@@ -1,44 +1,7 @@
 import * as React from 'react';
-// import {connect} from 'react-redux';
-// import {bindActionCreators, Dispatch} from 'redux';
 
-// import {RootState} from '../../reducers/rootReducer';
-// import {authActionCreator} from '../../actions/Auth/authActionCreator';
 import LeafletMap from '../../components/LeafletMap/LeafletMap';
 
-// interface StateProps {
-// isLoggedIn: boolean;
-// }
+const LeafletMapContainer: React.FC<{}> = () => <LeafletMap />;
 
-// interface DispatchProps {
-//   getIsLoggedIn: () => void;
-// }
-
-// type EnhancedIndexProps = LeafletMapProps & StateProps;
-
-// const mapStateToProps = (state: RootState): StateProps => ({
-//   isLoggedIn: state.authState.isLoggedIn,
-// });
-
-// const mapDispatchToProps = (dispatch: Dispatch): DispatchProps =>
-//   bindActionCreators(
-//     {
-//       getIsLoggedIn: () => authActionCreator.login(),
-//     },
-//     dispatch,
-//   );
-
-const LeafletMapContainer: React.FC<{}> = () => {
-  // if (!isLoggedIn) {
-  //   window.location.href = '/auth';
-  // }
-
-  return (
-    <div>
-      <LeafletMap />
-    </div>
-  );
-};
-
-// export default connect(mapStateToProps, mapDispatchToProps)(LeafletMapContainer);
 export default LeafletMapContainer;
--- src/sagas/auth/index.ts
+++ src/sagas/auth/index.ts
@@ -2,10 +2,6 @@ import {all, fork} from 'redux-saga/effects';
 
 import {watchGetIsAuthorised} from './watchers';
 
-/**
- * When the application starts,
- * the Saga Middleware executes RootSaga() to invoke watchGetIsAuthorised().
- */
 export default function* rootSaga() {
   yield all([fork(watchGetIsAuthorised)]);
 }

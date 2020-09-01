087522edb795ff744da4c197c3d9459fa7c757ea
xyzsince2014 <xyzsince2014@gmail.com>
2020-09-02 00:26:30 +0900

    refactored SignIn component

--- src/components/Auth/SignIn.tsx
+++ src/components/Auth/SignIn.tsx
@@ -1,21 +1,10 @@
 import * as React from 'react';
 import {AiFillTwitterCircle} from 'react-icons/ai';
-import {FaFacebook} from 'react-icons/fa';
 
-/**
- * call /auth/twitter for authentication
- * the result will be stored in the cookie
- */
 const SignIn: React.FC<{}> = () => (
-  <div>
-    <a href="/">
-      <FaFacebook />
-    </a>
-    {/* <a href={`${process.env.DOMAIN_API_AUTH}/auth/twitter`}> */}
-    <a href={`${process.env.DOMAIN_API_AUTH}/auth/twitter`}>
-      <AiFillTwitterCircle />
-    </a>
-  </div>
+  <a href={`${process.env.DOMAIN_API_AUTH}/auth/twitter`}>
+    <AiFillTwitterCircle />
+  </a>
 );
 
 export default SignIn;

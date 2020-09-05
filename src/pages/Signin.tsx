32e6a7f901e9a0043139fec641503d7179548267
xyzsince2014 <xyzsince2014@gmail.com>
2020-09-05 21:22:18 +0900

    modified signin page design

--- src/pages/Signin.tsx
+++ src/pages/Signin.tsx
@@ -1,17 +1,17 @@
 import * as React from 'react';
 
 import ListSignin from '../components/Signin/ListSignin';
-import MainVisual from '../assets/images/main-visual-pc.jpg';
+import Clock from '../containers/Clock/Clock';
 
 const SignIn: React.FC = () => (
   <div className="l-partition">
     <div className="l-partition__main">
-      <img src={MainVisual} alt="Main Visual" className="l-partition__main__background" />
       <div className="l-partition__main__content">
-        <div className="l-wrapper">
-          <ListSignin />
-        </div>
+        <ListSignin />
       </div>
+      <footer className="l-partition__footer">
+        <Clock />
+      </footer>
     </div>
   </div>
 );

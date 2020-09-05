f2a376341f20d8da652aa1ef35ec505f6873d82b
xyzsince2014 <xyzsince2014@gmail.com>
2020-09-05 11:59:26 +0900

    added line signin btn

--- src/components/Signin/ListSignin.tsx
+++ src/components/Signin/ListSignin.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import TwitterLogo from '../../assets/images/icons/logo_tw.png';
 import FacebookLogo from '../../assets/images/icons/logo_fb.png';
+import LineLogo from '../../assets/images/icons/logo_line.png';
 
 const ListSignin = () => (
   /* eslint-disable jsx-a11y/click-events-have-key-events */
@@ -23,12 +24,25 @@ const ListSignin = () => (
         role="button"
         tabIndex={0}
         onClick={() => {
-          window.location.href = `${process.env.DOMAIN_API_AUTH}/auth/twitter`;
+          window.location.href = `#`;
         }}
         className="c-social-signin-btn__content"
       >
         <img src={FacebookLogo} alt="Sign in with Facebook" />
         <span>Sign in with Facebook</span>
+      </div>
+    </li>
+    <li className="c-social-signin-btn c-social-signin-btn--line" key={3}>
+      <div
+        role="button"
+        tabIndex={0}
+        onClick={() => {
+          window.location.href = `#`;
+        }}
+        className="c-social-signin-btn__content"
+      >
+        <img src={LineLogo} alt="Sign in with Facebook" />
+        <span>Sign in with Line</span>
       </div>
     </li>
   </ul>
--- src/pages/Signin.tsx
+++ src/pages/Signin.tsx
@@ -1,12 +1,16 @@
 import * as React from 'react';
 
 import ListSignin from '../components/Signin/ListSignin';
+import MainVisual from '../assets/images/main-visual-pc.jpg';
 
 const SignIn: React.FC = () => (
   <div className="l-partition">
     <div className="l-partition__main">
-      <div className="l-wrapper">
-        <ListSignin />
+      <img src={MainVisual} alt="Main Visual" className="l-partition__main__background" />
+      <div className="l-partition__main__content">
+        <div className="l-wrapper">
+          <ListSignin />
+        </div>
       </div>
     </div>
   </div>

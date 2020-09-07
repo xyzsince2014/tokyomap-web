2e2beb09775a3b0438183bf685a2847c2928b575
xyzsince2014 <xyzsince2014@gmail.com>
2020-09-07 15:58:35 +0900

    disabled Particle.js

--- src/pages/Signin.tsx
+++ src/pages/Signin.tsx
@@ -1,28 +1,28 @@
 import * as React from 'react';
-import Particles, {IParticlesParams} from 'react-particles-js';
+// import Particles, {IParticlesParams} from 'react-particles-js';
 
 import ListSignin from '../components/Signin/ListSignin';
 import Clock from '../containers/Clock/Clock';
 
 /* eslint-disable @typescript-eslint/camelcase */
-const particleConfig: IParticlesParams = {
-  interactivity: {
-    detect_on: 'window',
-    events: {
-      onhover: {
-        enable: true,
-        mode: 'grab',
-      },
-      resize: true,
-    },
-  },
-};
+// const particleConfig: IParticlesParams = {
+//   interactivity: {
+//     detect_on: 'window',
+//     events: {
+//       onhover: {
+//         enable: true,
+//         mode: 'grab',
+//       },
+//       resize: true,
+//     },
+//   },
+// };
 /* eslint-enable @typescript-eslint/camelcase */
 
 const SignIn: React.FC = () => (
   <div className="l-partition">
     <div className="l-partition__main">
-      <Particles params={particleConfig} />
+      {/* <Particles params={particleConfig} /> */}
       <div className="l-partition__main__content">
         <ListSignin />
       </div>

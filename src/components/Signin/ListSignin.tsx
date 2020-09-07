b8ea1617acc68b73dbf4f00d93122ad98e5f26fe
Ryuta Yamamoto <30502252+xyzsince2014@users.noreply.github.com>
2020-09-07 18:07:46 +0900

    refactored signin page (#9)
    * refactored Signin component
    
    * refactored Signin component
    
    * refactored Signin component
    
    * added line signin btn
    
    * modified signin page design
    
    * replaced images & icons in Signin page
    
    * added particle.js
    
    * replaced background image on sign page
    
    * disabled Particle.js

---
+++ src/components/Signin/ListSignin.tsx
@@ -0,0 +1,66 @@
+import * as React from 'react';
+import TwitterLogo from '../../assets/images/icons/logo_tw.png';
+import FacebookLogo from '../../assets/images/icons/logo_fb.png';
+import LineLogo from '../../assets/images/icons/logo_line.png';
+import TelegramLogo from '../../assets/images/icons/logo_telegram.png';
+
+const ListSignin = () => (
+  /* eslint-disable jsx-a11y/click-events-have-key-events */
+  <ul className="l-social-signin">
+    <li className="c-social-signin-btn c-social-signin-btn--twitter" key={1}>
+      <div
+        role="button"
+        tabIndex={0}
+        onClick={() => {
+          window.location.href = `${process.env.DOMAIN_API_AUTH}/auth/twitter`;
+        }}
+        className="c-social-signin-btn__content"
+      >
+        <img src={TwitterLogo} alt="Sign in with Twitter" />
+        <span>Sign in with Twitter</span>
+      </div>
+    </li>
+    <li className="c-social-signin-btn c-social-signin-btn--facebook" key={2}>
+      <div
+        role="button"
+        tabIndex={0}
+        onClick={() => {
+          window.location.href = `#`;
+        }}
+        className="c-social-signin-btn__content"
+      >
+        <img src={FacebookLogo} alt="Sign in with Facebook" />
+        <span>Sign in with Facebook</span>
+      </div>
+    </li>
+    <li className="c-social-signin-btn c-social-signin-btn--line" key={3}>
+      <div
+        role="button"
+        tabIndex={0}
+        onClick={() => {
+          window.location.href = `#`;
+        }}
+        className="c-social-signin-btn__content"
+      >
+        <img src={LineLogo} alt="Sign in with Line" />
+        <span>Sign in with Line</span>
+      </div>
+    </li>
+    <li className="c-social-signin-btn c-social-signin-btn--telegram" key={4}>
+      <div
+        role="button"
+        tabIndex={0}
+        onClick={() => {
+          window.location.href = `#`;
+        }}
+        className="c-social-signin-btn__content"
+      >
+        <img src={TelegramLogo} alt="Sign in with Telegram" />
+        <span>Sign in with Telegram</span>
+      </div>
+    </li>
+  </ul>
+);
+/* eslint-enable jsx-a11y/click-events-have-key-events */
+
+export default ListSignin;
---
+++ src/pages/Signin.tsx
@@ -0,0 +1,36 @@
+import * as React from 'react';
+// import Particles, {IParticlesParams} from 'react-particles-js';
+
+import ListSignin from '../components/Signin/ListSignin';
+import Clock from '../containers/Clock/Clock';
+
+/* eslint-disable @typescript-eslint/camelcase */
+// const particleConfig: IParticlesParams = {
+//   interactivity: {
+//     detect_on: 'window',
+//     events: {
+//       onhover: {
+//         enable: true,
+//         mode: 'grab',
+//       },
+//       resize: true,
+//     },
+//   },
+// };
+/* eslint-enable @typescript-eslint/camelcase */
+
+const SignIn: React.FC = () => (
+  <div className="l-partition">
+    <div className="l-partition__main">
+      {/* <Particles params={particleConfig} /> */}
+      <div className="l-partition__main__content">
+        <ListSignin />
+      </div>
+      <footer className="l-partition__footer">
+        <Clock />
+      </footer>
+    </div>
+  </div>
+);
+
+export default SignIn;

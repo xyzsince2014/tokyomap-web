16c67746545d0e9fa742dfad8c591aac69173940
xyzsince2014 <xyzsince2014@gmail.com>
2020-10-13 19:52:49 +0900

    renamed the auth task

--- src/sagas/auth/tasks.ts
+++ src/sagas/auth/tasks.ts
@@ -13,10 +13,10 @@ import {put, call} from 'redux-saga/effects';
 import * as authActions from '../../actions/Auth/authActionCreator';
 import * as api from '../../services/auth/api';
 
-export function* runGetIsAuthorised(action: ReturnType<typeof authActions.authenticate.begin>) {
+export function* runAuthenticate(action: ReturnType<typeof authActions.authenticate.begin>) {
   try {
-    const getIsAuthorised = api.getAuthFactory();
-    const {isAuthenticated, userId} = yield call(getIsAuthorised);
+    const authenticate = api.getAuthFactory();
+    const {isAuthenticated, userId} = yield call(authenticate);
     yield put(authActions.authenticate.resolve({isAuthenticated, userId}));
   } catch (err) {
     yield put(authActions.authenticate.reject(err));

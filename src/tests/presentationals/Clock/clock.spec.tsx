96333685bbe04de674fd0721d1010e46c7d7ae11
xyzsince2014 <xyzsince2014@gmail.com>
2020-11-08 19:21:41 +0900

    updated Clock test script

--- src/tests/presentationals/Clock/clock.spec.tsx
+++ src/tests/presentationals/Clock/clock.spec.tsx
@@ -4,7 +4,7 @@ import renderer from 'react-test-renderer';
 import Clock from '../../../presentationals/Clock/Clock';
 
 describe('Clock', () => {
-  const component = renderer.create(<Clock datetime="hoge" />);
+  const component = renderer.create(<Clock datetime="8 Nov 19:20" />);
   const clockJson = component.toJSON();
 
   test('render Clock', () => {

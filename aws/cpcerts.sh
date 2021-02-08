904ea6252a77f4c01d6c26409cdf1864e727663c
xyzsince2014 <xyzsince2014@gmail.com>
2021-02-08 20:10:04 +0900

    added certbot certification

---
+++ aws/cpcerts.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cp -r /etc/letsencrypt/archive/www.tokyomap.live/cert1.pem ./certs/cert.pem
+cp -r /etc/letsencrypt/archive/www.tokyomap.live/fullchain1.pem ./certs/fullchain.pem
+cp -r /etc/letsencrypt/archive/www.tokyomap.live/privkey1.pem ./certs/privkey.pem
---
+++ aws/mkcerts.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# use ALB's fixed response returning to issue the certificates
+sudo certbot certonly --manual --domain www.tokyomap.live
